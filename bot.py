e412a4598687f20b6df1f167ff412ee19ca9c3b9
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2024-01-05 01:03:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,11 +17,31 @@ if __name__ == "__main__" :
     # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
-    plugins = dict(root="plugins")
-    JNbot = JNbot(
-        "VS Bot",
-        bot_token=Config.BOT_TOKEN,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins)
-    JNbot.run()
+    Bot.run()
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="VS Bot",
+            bot_token=Config.BOT_TOKEN,
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            plugins={
+                "root": "plugins"
+            }
+        )
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.uptime = datetime.now()
+
+       
+        #web-response
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
+
+    async def stop(self, *args):
+        await super().stop()

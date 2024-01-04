17516190f03889e83317d37aa3add3229c7f705e
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2024-01-05 00:07:51 +0530

    Update and rename split.py to cb.py

---
+++ plugins/cb.py
@@ -0,0 +1,18 @@
+import os, shutil
+from pyrogram import Client, types
+from plugins.stuff import parts_handler
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+@Client.on_callback_query()
+async def button(bot, update):
+    
+    if update.data == "close":
+        await update.message.delete(True)
+        if os.path.isdir(parts_handler.file_folder):
+            shutil.rmtree(parts_handler.file_folder)
+    else:
+        pass

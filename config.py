71dc0af1d05b4996efa9b0bed4849f13eca9f635
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2024-01-05 01:52:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ logging.basicConfig(
 
 class Config(object):
 
-    # BOT_TOKEN = os.environ.get("BOT_TOKEN", "6100891233:AAHo_OjnFWTdY_JewRdcqphxASAcAK1IHVg") #ajhhsa
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5872747581:AAH7_XPCOCEVfbgUhepjJWlcOmj8wjDTjBk") #jn_url
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6100891233:AAHo_OjnFWTdY_JewRdcqphxASAcAK1IHVg") #ajhhsa
+    #BOT_TOKEN = os.environ.get("BOT_TOKEN", "5872747581:AAH7_XPCOCEVfbgUhepjJWlcOmj8wjDTjBk") #jn_url
 
     API_ID = int(os.environ.get("API_ID", "3393749"))
 

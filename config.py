a0eff0c9e7e77683200cb366b6eafbc82723e045
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2024-01-05 01:13:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,27 +28,9 @@ class Config(object):
 
     UPDATES_CHANNEL = os.environ.get("UPDATE_CHANNEL", "-1001512853438")
 
-    MAX_FILE_SIZE = 4194304000
-
-    TG_MAX_FILE_SIZE = 4194304000
-
-    FREE_USER_MAX_FILE_SIZE = 4194304000
-
-    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
-
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
 
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
-
-    OUO_IO_API_KEY = ""
-
-    MAX_MESSAGE_LENGTH = 4096
-
-    PROCESS_MAX_TIMEOUT = 0
-
-    DEF_WATER_MARK_FILE = "Use this bot @url_uploaderV3Bott"
-
-    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Jayanna:Jayanna2023@yash.tm1c2bd.mongodb.net/?retryWrites=true&w=majority")
 
     SESSION_NAME = os.environ.get("SESSION_NAME", "TG_FILES")
 
@@ -58,8 +40,7 @@ class Config(object):
 
     OWNER_ID = int(os.environ.get("OWNER_ID", "1061576483"))
     
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "dgfghgjbot")
-
-    PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "1061576483").split()))
+    #Port
+    PORT = os.environ.get("PORT", "8080")
 
     PRO_USERS.append(OWNER_ID)

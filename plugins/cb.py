27e1b3102de9fa7d34532a344c93811bf8f08b5b
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2024-01-05 14:57:26 +0530

    Update cb.py

--- plugins/cb.py
+++ plugins/cb.py
@@ -13,10 +13,8 @@ logger = logging.getLogger(__name__)
 
 @Client.on_callback_query()
 async def button(bot, update):
-    
     if update.data == "close":
         await update.message.delete(True)
-
     elif update.data == "home":
         await update.message.edit_text(
             text=Config.START_TEXT.format(update.from_user.mention),

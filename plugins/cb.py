80374474eeecb5e35c9a6f054e741c998fb52a4d
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2024-01-05 10:51:35 +0530

    Update cb.py

--- plugins/cb.py
+++ plugins/cb.py
@@ -1,6 +1,7 @@
 #Coded by KA18 the @legend580 💛❤️
 
 import os, shutil
+from config import Config
 from pyrogram import Client, types
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import logging
@@ -13,5 +14,45 @@ async def button(bot, update):
     
     if update.data == "close":
         await update.message.delete(True)
+
+    elif update.data == "home":
+        await update.message.edit_text(
+            text=Config.START_TEXT.format(update.from_user.mention),
+            reply_markup=Config.START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=Config.HELP_TEXT,
+            reply_markup=Config.HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=Config.ABOUT_TEXT,
+            reply_markup=Config.ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+    #elif update.data == "showThumbnail":
+        #thumbnail = await db.get_thumbnail(update.from_user.id)
+        #if not thumbnail:
+            #await update.answer("You didn't set any custom thumbnail!", show_alert=True)
+        #else:
+            #await update.answer()
+            #await bot.send_photo(update.message.chat.id, thumbnail, "Custom Thumbnail",
+                               #reply_markup=types.InlineKeyboardMarkup([[
+                                   #types.InlineKeyboardButton("Delete Thumbnail",
+                                                              callback_data="deleteThumbnail")
+                               #]]))
+    #elif update.data == "deleteThumbnail":
+        #await db.set_thumbnail(update.from_user.id, None)
+        #await update.answer("Okay, I deleted your custom thumbnail. Now I will apply default thumbnail.", show_alert=True)
+        #await update.message.delete(True)
+    #elif update.data == "setThumbnail":
+        #await update.message.edit_text(
+            #text=Translation.TEXT,
+            #reply_markup=Translation.BUTTONS,
+            #disable_web_page_preview=True
+        #)
     else:
         pass

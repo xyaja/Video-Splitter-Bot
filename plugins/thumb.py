6240cba746d52788eab3053d5f1f35f5eeb64c5f
Jay <92670065+Jaya-G-Gowda@users.noreply.github.com>
2024-01-05 16:02:20 +0530

    Update thumb.py

--- plugins/thumb.py
+++ plugins/thumb.py
@@ -26,7 +26,7 @@ from plugins.database.add import add_user_to_database
 from plugins.settings.settings import *
 
 
-@Client.on_message(filters.private & filters.photo & ~filters.edited)
+@Client.on_message(filters.private & filters.photo)
 async def photo_handler(bot: Client, event: Message):
     if not event.from_user:
         return await event.reply_text("I don't know about you sar :(")
@@ -36,7 +36,7 @@ async def photo_handler(bot: Client, event: Message):
     await editable.edit("**✅ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ sᴀᴠᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ!!**")
 
 
-@Client.on_message(filters.private & filters.command(["delthumb", "deletethumbnail"]) & ~filters.edited)
+@Client.on_message(filters.private & filters.command(["delthumb", "deletethumbnail"]))
 async def delete_thumb_handler(bot: Client, event: Message):
     if not event.from_user:
         return await event.reply_text("I don't know about you sar :(")
@@ -49,7 +49,7 @@ async def delete_thumb_handler(bot: Client, event: Message):
         ])
     )
 
-@Client.on_message(filters.private & filters.command("showthumb") )
+@Client.on_message(filters.private & filters.command("showthumb"))
 async def viewthumbnail(bot, update):
     if not update.from_user:
         return await update.reply_text("I don't know about you sar :(")

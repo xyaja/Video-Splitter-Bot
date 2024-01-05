d763fd36d6fed6eb87c123c019539d62ff43ac56
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2024-01-05 13:38:07 +0530

    Create add.py

---
+++ database/add.py
@@ -0,0 +1,12 @@
+#Coded by KA18 the @legend580 💛❤️
+
+from plugins.config import Config
+from .database import db
+from pyrogram import Client
+from pyrogram.types import Message
+
+
+async def add_user_to_database(bot: Client, cmd: Message):
+    if not await db.is_user_exist(cmd.from_user.id):
+        await db.add_user(cmd.from_user.id)
+        
